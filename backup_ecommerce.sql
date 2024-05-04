b57f64521a1d94000f4e5474d01ef5eaa8c388d3
RobertRocha <119464289+RobertRocha@users.noreply.github.com>
2024-05-03 22:20:49 -0300

    Add files via upload

---
+++ backup_ecommerce.sql
@@ -0,0 +1,413 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ecommerce
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `ecommerce`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `ecommerce` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `ecommerce`;
+
+--
+-- Table structure for table `clients`
+--
+
+DROP TABLE IF EXISTS `clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clients` (
+  `idCliente` int NOT NULL AUTO_INCREMENT,
+  `Fname` varchar(10) DEFAULT NULL,
+  `Minit` char(3) DEFAULT NULL,
+  `Lname` varchar(20) DEFAULT NULL,
+  `CPF` char(11) NOT NULL,
+  `address` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`idCliente`),
+  UNIQUE KEY `unique_cpf_client` (`CPF`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clients`
+--
+
+LOCK TABLES `clients` WRITE;
+/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
+INSERT INTO `clients` VALUES (1,'Maria','M','Silva','12346789',' rua ouro 87, Carango '),(2,'Matheus','O','Pimentel','987654321',' rua manacas dias 78, Carango '),(3,'Ricardo','F','Silva','45678913','avenida alameda vinha 1009, Ce'),(4,'Julia','S','França','789123456','rua lareijras 861, Centro - Ci'),(5,'Roberta','G','Assis','98745631','avenida koller 19, Centro - Ci'),(6,'Izabela','M','Cruz','654789123','rua alameda das flores 28, Cen'),(7,'Mário','P','Pereira','546258795','rua tesouros 12, Canadá');
+/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `delivery`
+--
+
+DROP TABLE IF EXISTS `delivery`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `delivery` (
+  `idDelivey` int NOT NULL AUTO_INCREMENT,
+  `deliveryStatus` enum('Saindo','Em trânsito','Entregue') DEFAULT NULL,
+  `trackingNumber` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idDelivey`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `delivery`
+--
+
+LOCK TABLES `delivery` WRITE;
+/*!40000 ALTER TABLE `delivery` DISABLE KEYS */;
+INSERT INTO `delivery` VALUES (1,'Em trânsito','12345'),(2,'Entregue','123456');
+/*!40000 ALTER TABLE `delivery` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `deliveryrequest`
+--
+
+DROP TABLE IF EXISTS `deliveryrequest`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `deliveryrequest` (
+  `idDeliveryRequest` int NOT NULL AUTO_INCREMENT,
+  `id_delivery` int DEFAULT NULL,
+  `id_request` int DEFAULT NULL,
+  PRIMARY KEY (`idDeliveryRequest`),
+  KEY `fk_delivery_delivery` (`id_delivery`),
+  KEY `fk_request_orders` (`id_request`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `deliveryrequest`
+--
+
+LOCK TABLES `deliveryrequest` WRITE;
+/*!40000 ALTER TABLE `deliveryrequest` DISABLE KEYS */;
+INSERT INTO `deliveryrequest` VALUES (1,1,1),(2,2,2);
+/*!40000 ALTER TABLE `deliveryrequest` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `idOrder` int NOT NULL AUTO_INCREMENT,
+  `idOrderClient` int DEFAULT NULL,
+  `orderStatus` enum('Cancelado','Confirmado','Em processamento') DEFAULT 'Em processamento',
+  `orderDescription` varchar(255) DEFAULT NULL,
+  `sendValue` float DEFAULT '10',
+  `PaymentCash` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`idOrder`),
+  KEY `fk_orders_client` (`idOrderClient`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,1,'Em processamento','compra via aplicativo',NULL,1),(2,2,'Em processamento','compra via aplicativo',50,0),(3,3,'Confirmado',NULL,NULL,1),(4,4,'Em processamento','compra via web site ',150,0),(5,2,'Em processamento','compra via aplicativo',NULL,1);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `payments`
+--
+
+DROP TABLE IF EXISTS `payments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `payments` (
+  `idPayment` int DEFAULT NULL,
+  `typepayment` enum('Boleto','cartão','Dois cartões') DEFAULT NULL,
+  `id_client` int DEFAULT NULL,
+  KEY `fk_payments_client` (`id_client`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `payments`
+--
+
+LOCK TABLES `payments` WRITE;
+/*!40000 ALTER TABLE `payments` DISABLE KEYS */;
+INSERT INTO `payments` VALUES (1,'cartão',1),(2,'Boleto',4);
+/*!40000 ALTER TABLE `payments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `idProduct` int NOT NULL AUTO_INCREMENT,
+  `Pname` varchar(10) NOT NULL,
+  `classification_kids` tinyint(1) DEFAULT '0',
+  `category` enum('Eletrônico','Vestimenta','Brinquedos','Alimentos','Móveis') NOT NULL,
+  `avaliação` float DEFAULT '0',
+  `size` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`idProduct`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'Fone de ou',0,'Eletrônico',4,NULL),(2,'Barbie Els',1,'Brinquedos',3,NULL),(3,'Body Carte',1,'Vestimenta',5,NULL),(4,'Microfone ',0,'Eletrônico',4,NULL),(5,'Sofá retrá',0,'Móveis',3,'3x57x80'),(6,'Farinha de',0,'Alimentos',2,NULL);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productorder`
+--
+
+DROP TABLE IF EXISTS `productorder`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productorder` (
+  `idPOproduct` int NOT NULL,
+  `idPOorder` int NOT NULL,
+  `poQuantity` int DEFAULT '1',
+  `poStatus` enum('Disponível','Sem estoque') DEFAULT 'Disponível',
+  PRIMARY KEY (`idPOproduct`,`idPOorder`),
+  KEY `fk_productorder_product` (`idPOorder`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productorder`
+--
+
+LOCK TABLES `productorder` WRITE;
+/*!40000 ALTER TABLE `productorder` DISABLE KEYS */;
+INSERT INTO `productorder` VALUES (1,1,2,NULL),(2,1,1,NULL),(3,2,1,NULL);
+/*!40000 ALTER TABLE `productorder` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productseller`
+--
+
+DROP TABLE IF EXISTS `productseller`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productseller` (
+  `idPseller` int NOT NULL,
+  `idPproduct` int NOT NULL,
+  `prodQuantity` int DEFAULT '1',
+  PRIMARY KEY (`idPseller`,`idPproduct`),
+  KEY `fk_product_product` (`idPproduct`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productseller`
+--
+
+LOCK TABLES `productseller` WRITE;
+/*!40000 ALTER TABLE `productseller` DISABLE KEYS */;
+INSERT INTO `productseller` VALUES (1,6,80),(2,7,10);
+/*!40000 ALTER TABLE `productseller` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productstorage`
+--
+
+DROP TABLE IF EXISTS `productstorage`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productstorage` (
+  `idProdStorage` int NOT NULL AUTO_INCREMENT,
+  `storageLocation` varchar(255) DEFAULT NULL,
+  `quantity` int DEFAULT '0',
+  PRIMARY KEY (`idProdStorage`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productstorage`
+--
+
+LOCK TABLES `productstorage` WRITE;
+/*!40000 ALTER TABLE `productstorage` DISABLE KEYS */;
+INSERT INTO `productstorage` VALUES (1,'Rio de Janeiro',1000),(2,'Rio de Janeiro',500),(3,'São Paulo',10),(4,'São Paulo',100),(5,'São Paulo',10),(6,'Brasília',60);
+/*!40000 ALTER TABLE `productstorage` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productsupplier`
+--
+
+DROP TABLE IF EXISTS `productsupplier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productsupplier` (
+  `idPsSupplier` int NOT NULL,
+  `idPsProduct` int NOT NULL,
+  `quantity` int NOT NULL,
+  PRIMARY KEY (`idPsSupplier`,`idPsProduct`),
+  KEY `fk_product_supplier_product` (`idPsProduct`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productsupplier`
+--
+
+LOCK TABLES `productsupplier` WRITE;
+/*!40000 ALTER TABLE `productsupplier` DISABLE KEYS */;
+INSERT INTO `productsupplier` VALUES (1,1,500),(1,2,400),(2,4,633),(3,3,5),(2,5,10);
+/*!40000 ALTER TABLE `productsupplier` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `register`
+--
+
+DROP TABLE IF EXISTS `register`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `register` (
+  `idRegister` int NOT NULL AUTO_INCREMENT,
+  `typeRegister` enum('CPF','CNPJ') DEFAULT NULL,
+  `numbers` varchar(20) DEFAULT NULL,
+  `id_client` int DEFAULT NULL,
+  PRIMARY KEY (`idRegister`),
+  KEY `fk_register_client` (`id_client`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `register`
+--
+
+LOCK TABLES `register` WRITE;
+/*!40000 ALTER TABLE `register` DISABLE KEYS */;
+INSERT INTO `register` VALUES (1,'CPF','12346789',1),(2,'CNPJ','124778210000198',4);
+/*!40000 ALTER TABLE `register` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `seller`
+--
+
+DROP TABLE IF EXISTS `seller`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `seller` (
+  `idSeller` int NOT NULL AUTO_INCREMENT,
+  `socialName` varchar(255) NOT NULL,
+  `abstName` varchar(255) DEFAULT NULL,
+  `CNPJ` char(15) DEFAULT NULL,
+  `CPF` char(9) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `contact` char(11) NOT NULL,
+  PRIMARY KEY (`idSeller`),
+  UNIQUE KEY `unique_cnpj_seller` (`CNPJ`),
+  UNIQUE KEY `unique_cpf_seller` (`CPF`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `seller`
+--
+
+LOCK TABLES `seller` WRITE;
+/*!40000 ALTER TABLE `seller` DISABLE KEYS */;
+INSERT INTO `seller` VALUES (1,'Tech eletronics',NULL,'123456789456321',NULL,'Rio de Janeiro','219946287'),(2,'Botique Durgas',NULL,NULL,'123456783','Rio de Janeiro','219567895'),(3,'Kids Word',NULL,'456789123654485',NULL,'São Paulo','1198657484');
+/*!40000 ALTER TABLE `seller` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `storagelocation`
+--
+
+DROP TABLE IF EXISTS `storagelocation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `storagelocation` (
+  `idLproduct` int NOT NULL,
+  `idLstorage` int NOT NULL,
+  `location` varchar(255) NOT NULL,
+  PRIMARY KEY (`idLproduct`,`idLstorage`),
+  KEY `fk_storage_location_storage` (`idLstorage`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `storagelocation`
+--
+
+LOCK TABLES `storagelocation` WRITE;
+/*!40000 ALTER TABLE `storagelocation` DISABLE KEYS */;
+INSERT INTO `storagelocation` VALUES (1,2,'RJ'),(2,6,'GO');
+/*!40000 ALTER TABLE `storagelocation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `supplier`
+--
+
+DROP TABLE IF EXISTS `supplier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `supplier` (
+  `idSupplier` int NOT NULL AUTO_INCREMENT,
+  `socialName` varchar(255) NOT NULL,
+  `CNPJ` char(15) NOT NULL,
+  `contact` char(11) NOT NULL,
+  PRIMARY KEY (`idSupplier`),
+  UNIQUE KEY `unique_supplier` (`CNPJ`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `supplier`
+--
+
+LOCK TABLES `supplier` WRITE;
+/*!40000 ALTER TABLE `supplier` DISABLE KEYS */;
+INSERT INTO `supplier` VALUES (1,'Almeida e filhos','123456789123456','21985474'),(2,'Eletrônicos Silva','854519649143457','21985484'),(3,'Eletrônicos Valma','934567893934695','21975474');
+/*!40000 ALTER TABLE `supplier` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-02 22:05:53
